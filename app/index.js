21305b1b6fdb9432b577f414ec3c785740447d04
Pranav Garg <pranav.garg1981@gmail.com>
2021-02-28 12:07:30 +0530

    starting a sample react project

---
+++ app/index.js
@@ -0,0 +1,15 @@
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var App=
+// require('./components/App');
+//
+// ReactDOM.render(
+//   <App />,
+//   document.getElementByID('app');
+//
+// );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.js';  
+
+ReactDOM.render(<App />, document.getElementById('app'));
